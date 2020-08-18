08833999ed23ec5fc9f3ed5e4dea9e956830fc85
sayeedreza99 <sayeedreza99.com>
2020-08-18 21:08:08 +0600

    lyrics issue solved

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// const apiLink = "https://api.lyrics.ovh";
+
 const userInput = document.querySelector(".form-control");
 const serachButton = document.querySelector(".search-btn");
 const searchResult = document.getElementById('searchResult');
@@ -5,6 +7,7 @@ const searchResult = document.getElementById('searchResult');
 serachButton.addEventListener('click', function () {
     if (userInput.value == "") {
         searchResult.innerText = "";
+        alert('nothing to search')
     }
     else if (userInput.value != "") {
         doS(userInput);
@@ -23,25 +26,52 @@ function doS(userInput) {
 }
 
 function doSomething(data) {
-
     searchResult.innerHTML = " ";
     for (let i = 0; i < 8; i++) {
         const element = data.data[i];
-        const title = element.album.title;
+        const title = element.title;
         const artist = element.artist.name;
+        const image = element.artist.picture_small;
+        const albumTitle = element.album.title;
+
+
         // console.log(artist);
         const child = `<div class="single-result row align-items-center my-3 p-3">
-        <div class="col-md-9">
+        <div class="col-md-8">
             <h3 class="lyrics-name">${title}</h3>
             <p class="author lead">Album by <span>${artist}</span></p>
+            <p class="author lead">Album Title :  <span id="artistName">${albumTitle}</span></p>
         </div>
+        <div class="col-md-1">
+                <img src="${image}" alt="">
+            </div>
         <div class="col-md-3 text-md-right text-center">
-            <button class="btn btn-success">Get Lyrics</button>
+            <button onclick="lyricsSearch('${artist}','${title}')" class="btn btn-success">Get Lyrics</button>
         </div>
         </div>`
 
         searchResult.innerHTML += child;
-
     }
+}
 
+function lyricsSearch(artist, title) {
+    fetch(`https://api.lyrics.ovh/v1/${artist}/${title}/`)
+        .then(response => response.json())
+        .then(data => showLyrics(data, title))
+        .catch(error => alert('error'))
+    // console.log(data)
+}
+
+function showLyrics(data, title) {
+    if (data.lyrics == undefined) {
+        document.querySelector('.single-lyrics').innerText =
+
+            ".....................NO LYRICS AVAILABLE............";
+    } else {
+        document.querySelector('.single-lyrics').innerHTML =
+            `<h2 class="text-success mb-4">Title - ${title}</h2>
+            <pre class="lyric text-white">
+            ${data.lyrics}
+            </pre>`;
+    }
 }
